564b177109c3e18e1631ff8e0faf5dfa8891a853
alenaPy <alena.sheshko@gmail.com>
2017-05-06 05:59:04 +0300

    Replace bulk insert with few sql-statements

--- src/ggrc_risks/migrations/versions/20170502140636_377d935e1b21_migrate_urls_to_documents.py
+++ src/ggrc_risks/migrations/versions/20170502140636_377d935e1b21_migrate_urls_to_documents.py
@@ -9,19 +9,15 @@ Create Date: 2017-05-02 14:06:36.936410
 # disable Invalid constant name pylint warning for mandatory Alembic variables.
 # pylint: disable=invalid-name
 
-from datetime import datetime
 from alembic import op
-from sqlalchemy import Column, Integer, String, DateTime
-from sqlalchemy.ext.automap import automap_base
-from sqlalchemy.orm import Session
-from sqlalchemy.sql import table, column, text
+from sqlalchemy import Column
+from sqlalchemy import String
 
 
 # revision identifiers, used by Alembic.
 revision = '377d935e1b21'
 down_revision = '178f7d2fb85e'
 
-Base = automap_base()
 
 HYPERLINKED_OBJECTS = {
     'Risk': 'risks',
@@ -29,100 +25,157 @@ HYPERLINKED_OBJECTS = {
 }
 
 
-def _get_relationships(connection, object_name, table_name):
+def _create_documents(connection, object_type, table_name):
   """Create documents and prepare relationships for insert."""
-  urls_to_migrate = connection.execute(
+
+  connection.execute(
       """
-          SELECT id, url FROM {table_name}
+          INSERT INTO temp_url (object_id, url, modified_by_id)
+          SELECT id, url, modified_by_id
+          FROM {table_name}
           WHERE url > ''
           UNION
-          SELECT id, reference_url FROM {table_name}
+          SELECT id, reference_url, modified_by_id
+          FROM {table_name}
           WHERE reference_url > ''
       """.format(table_name=table_name)
   )
 
-  Document = Base.classes.documents
+  connection.execute(
+      """ INSERT INTO documents (modified_by_id, title, link, created_at, updated_at)
+          SELECT modified_by_id, url, url, NOW(), NOW()
+          FROM temp_url
+          ORDER BY id
+      """
+  )
 
-  session = Session(connection)
-  # workaround for issue fixed in Flask-SQLAlchemy v2.0
-  # pylint: disable=W0212
-  session._model_changes = {}
+  last_id = int(connection.execute('SELECT LAST_INSERT_ID()').first()[0])
 
-  now = datetime.now()
-  relationships_to_add = []
-  for object_id, url in urls_to_migrate:
-    doc = Document(
-        created_at=now,
-        updated_at=now,
-        title=url,
-        link=url,
-        document_type='REFERENCE_URL'
+  if last_id:
+    connection.execute(
+        """
+            INSERT INTO relationships (modified_by_id,
+                                        source_id,
+                                        source_type,
+                                        destination_id,
+                                        destination_type,
+                                        created_at,
+                                        updated_at)
+            SELECT t.modified_by_id,
+                  t.object_id,
+                  '{object_type}',
+                  d.id,
+                  'Document',
+                  NOW(),
+                  NOW()
+            FROM temp_url t
+            JOIN documents d ON d.id = t.id + {delta}
+        """.format(object_type=object_type, delta=last_id - 1)
     )
-    session.add(doc)
-    session.commit()
 
-    relationships_to_add.append({
-        'source_id': object_id,
-        'source_type': object_name,
-        'destination_id': doc.id,
-        'destination_type': 'Document',
-        'created_at': now,
-        'updated_at': now
-    })
-  session.close()
-  return relationships_to_add
+  connection.execute('TRUNCATE TABLE temp_url')
 
 
 def upgrade():
   """Upgrade database schema and/or data, creating a new revision."""
   connection = op.get_bind()
 
-  # reflect the tables
-  Base.prepare(connection, reflect=True)
+  connection.execute("""
+      CREATE TEMPORARY TABLE temp_url (
+          id int(11) NOT NULL AUTO_INCREMENT,
+          object_id int(11) NOT NULL,
+          url varchar(250) NOT NULL,
+          modified_by_id int(11) DEFAULT NULL,
+          PRIMARY KEY (id)
+      )
+  """)
 
-  relationships = table(
-      'relationships',
-      column('source_id', Integer),
-      column('source_type', String),
-      column('destination_id', Integer),
-      column('destination_type', String),
-      column('created_at', DateTime),
-      column('updated_at', DateTime)
-  )
-
-  for object_name, table_name in HYPERLINKED_OBJECTS.iteritems():
-    op.bulk_insert(relationships,
-                   _get_relationships(connection, object_name, table_name))
+  for object_type, table_name in HYPERLINKED_OBJECTS.iteritems():
+    _create_documents(connection, object_type, table_name)
     op.drop_column(table_name, 'url')
     op.drop_column(table_name, 'reference_url')
+
+  connection.execute('DROP TABLE temp_url')
+
+
+def _migrate_ulrs_back(connection, object_type, table_name):
+  """Migrate url and reference_url back to an object table."""
+  connection.execute(
+      """
+          INSERT INTO temp_relationship (object_id, document_id)
+          SELECT source_id as object_id, destination_id as document_id
+          FROM relationships
+          WHERE source_type='{object_type}'
+            AND destination_type='Document'
+          UNION
+          SELECT destination_id as object_id, source_id as document_id
+          FROM relationships
+          WHERE destination_type='{object_type}'
+            AND source_type='Document'
+      """.format(object_type=object_type))
+
+  connection.execute(
+      """
+          INSERT INTO temp_url (object_id, document_id)
+          SELECT object_id, MIN(document_id)
+          FROM temp_relationship
+          GROUP BY object_id
+      """)
+
+  connection.execute(
+      """
+          INSERT INTO temp_reference_url (object_id, document_id)
+          SELECT object_id, MIN(document_id)
+          FROM temp_relationship
+          WHERE document_id NOT IN (SELECT document_id FROM temp_url)
+          GROUP BY object_id
+      """)
+
+  connection.execute(
+      """
+          UPDATE {table_name} t
+          JOIN temp_url u ON t.id = u.object_id
+          JOIN documents d ON d.id = u.document_id
+          SET t.url = d.link
+      """.format(table_name=table_name))
+
+  connection.execute(
+      """
+          UPDATE {table_name} t
+          JOIN temp_reference_url u ON t.id = u.object_id
+          JOIN documents d ON d.id = u.document_id
+          SET t.reference_url = d.link
+      """.format(table_name=table_name))
+
+  connection.execute('TRUNCATE TABLE temp_relationship')
+  connection.execute('TRUNCATE TABLE temp_url')
+  connection.execute('TRUNCATE TABLE temp_reference_url')
 
 
 def downgrade():
   """Downgrade database schema and/or data back to the previous revision."""
   connection = op.get_bind()
 
-  update_url = """
-      UPDATE {table_name}
-      SET url=:url
-      WHERE id = :object_id AND url IS NULL
-  """
+  connection.execute("""
+      CREATE TEMPORARY TABLE temp_relationship (
+          object_id int(11) NOT NULL,
+          document_id int(11) NOT NULL
+      )
+  """)
 
-  update_reference_url = """
-      UPDATE {table_name}
-      SET reference_url=:url
-      WHERE id = :object_id AND reference_url IS NULL AND url IS NOT NULL
-  """
+  connection.execute("""
+      CREATE TEMPORARY TABLE temp_url (
+          object_id int(11) NOT NULL,
+          document_id int(11) NOT NULL
+      )
+  """)
 
-  delete_reletionships = """
-       DELETE FROM relationships
-       WHERE destination_type = 'Document'
-         AND modified_by_id IS NULL
-  """
-
-  delete_document = """
-      DELETE FROM documents
-      WHERE id = {document_id}
-  """
+  connection.execute("""
+      CREATE TEMPORARY TABLE temp_reference_url (
+          object_id int(11) NOT NULL,
+          document_id int(11) NOT NULL
+      )
+  """)
 
   for object_name, table_name in HYPERLINKED_OBJECTS.iteritems():
     op.add_column(table_name,
@@ -130,30 +183,41 @@ def downgrade():
     op.add_column(table_name,
                   Column('reference_url', String(length=250), nullable=True))
 
-    urls_to_migrate = connection.execute(
-        """
-            SELECT r.source_id, d.link, d.id  FROM documents d
-            JOIN relationships r ON r.destination_id = d.id
-            WHERE r.source_type = '{object_name}'
-              AND r.destination_type = 'Document'
-              AND r.modified_by_id IS NULL
-              AND d.document_type = 'REFERENCE_URL'
-            ORDER BY d.id
-        """.format(object_name=object_name)
-    )
-
-    if not urls_to_migrate:
+    if object_name == 'Assessment':
       continue
 
-    for object_id, url, document_id in urls_to_migrate:
-      connection.execute(
-          text(update_reference_url.format(table_name=table_name)),
-          object_id=object_id,
-          url=url)
-      connection.execute(
-          text(update_url.format(table_name=table_name)),
-          object_id=object_id,
-          url=url)
-      connection.execute(delete_document.format(document_id=document_id))
+    _migrate_ulrs_back(connection, object_name, table_name)
 
-  connection.execute(delete_reletionships)
+  delete_reletionships = """
+      DELETE FROM relationships
+      WHERE (destination_type = 'Document'
+         AND source_type IN ({object_types}))
+         OR (destination_type IN ({object_types})
+         AND source_type = 'Document')
+  """
+
+  delete_document = """
+      DELETE FROM documents
+      WHERE id IN (SELECT destination_id
+                   FROM relationships
+                   WHERE destination_type = 'Document'
+                     AND source_type IN ({object_types})
+                   UNION
+                   SELECT source_id
+                   FROM relationships
+                   WHERE destination_type IN ({object_types})
+                     AND source_type = 'Document')
+  """
+
+  object_types_to_delete = ','.join(
+      ["'%s'" % obj for obj in HYPERLINKED_OBJECTS.iterkeys()
+       if obj != 'Assessment'])
+
+  connection.execute(delete_document.format(
+      object_types=object_types_to_delete))
+  connection.execute(delete_reletionships.format(
+      object_types=object_types_to_delete))
+
+  connection.execute('DROP TABLE temp_relationship')
+  connection.execute('DROP TABLE temp_url')
+  connection.execute('DROP TABLE temp_reference_url')
